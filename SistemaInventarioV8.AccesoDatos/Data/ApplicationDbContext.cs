fb27ca29764d8c79746cc82558cacc2f44c876bf
imonleonedam1 <ibaimonleon@gmail.com>
2024-08-13 09:55:44 +0200

    Configuración del proyecto

--- SistemaInventarioV8.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventarioV8.AccesoDatos/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventarioV8.Modelos;
+using System.Reflection;
 
 namespace SistemaInventarioV8.AccesoDatos.Data
 {
@@ -8,6 +10,14 @@ namespace SistemaInventarioV8.AccesoDatos.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
