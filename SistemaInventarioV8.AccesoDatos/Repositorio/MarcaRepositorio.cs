a5dd6386af2ac3c48ccf3ef3c894bbeb541ebe7a
imonleonedam1 <ibaimonleon@gmail.com>
2024-08-14 13:58:42 +0200

    CRUD de Marca

---
+++ SistemaInventarioV8.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using SistemaInventarioV8.AccesoDatos.Data;
+using SistemaInventarioV8.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV8.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV8.AccesoDatos.Repositorio
+{
+    public class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MarcaRepositorio(ApplicationDbContext db): base(db) 
+        {
+            _db = db;
+        }
+        public void Acualizar(Marca marca)
+        {
+            var marcaBD = _db.Marcas.FirstOrDefault(b => b.Id == marca.Id);
+
+            if (marcaBD != null) { 
+                marcaBD.Nombre = marca.Nombre;
+                marcaBD.Descripcion = marca.Descripcion;
+                marcaBD.Estado = marca.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
