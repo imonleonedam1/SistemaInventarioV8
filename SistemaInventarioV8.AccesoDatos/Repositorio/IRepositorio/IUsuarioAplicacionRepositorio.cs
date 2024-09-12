3bf542f0810e592d2b031a86b4f0d8ad50c756ce
imonleonedam1 <ibaimonleon@gmail.com>
2024-09-12 20:26:19 +0200

    Modelo Usuario

---
+++ SistemaInventarioV8.AccesoDatos/Repositorio/IRepositorio/IUsuarioAplicacionRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaInventarioV8.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV8.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IUsuarioAplicacionRepositorio : IRepositorio<UsuarioAplicacion>
+    {
+         
+    }
+}
---
+++ SistemaInventarioV8.AccesoDatos/Repositorio/UsuarioAplicacionRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using SistemaInventarioV8.AccesoDatos.Data;
+using SistemaInventarioV8.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV8.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV8.AccesoDatos.Repositorio
+{
+    public class UsuarioAplicacionRepositorio : Repositorio<UsuarioAplicacion>, IUsuarioAplicacionRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsuarioAplicacionRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
