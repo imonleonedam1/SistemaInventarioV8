1d6870f52a4abc3a399101c4298330c083072321
imonleonedam1 <ibaimonleon@gmail.com>
2024-09-14 10:58:35 +0200

    Administracion de usuarios

---
+++ SistemaInventarioV8.Utilidades/ErrorDescriber.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV8.Utilidades
+{
+    public class ErrorDescriber: IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError() { 
+            Code = nameof(PasswordRequiresLower),
+            Description = "La contraseña debe tene al menos una letra minúscula"
+            };
+        }
+    }
+}
---
+++ SistemaInventarioV8/Areas/Admin/Controllers/UsuarioController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventarioV8.AccesoDatos.Data;
+using SistemaInventarioV8.AccesoDatos.Repositorio.IRepositorio;
+using System.Data;
+
+namespace SistemaInventarioV8.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly ApplicationDbContext _db;
+
+        public UsuarioController(IUnidadTrabajo unidadTrabajo, ApplicationDbContext db)
+        {
+            _unidadTrabajo = unidadTrabajo;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var usuariosLista = await _unidadTrabajo.UsuarioAplicacion.ObtenerTodos();
+            var userRole = await _db.UserRoles.ToListAsync();
+            var roles = await _db.Roles.ToListAsync();
+
+            foreach (var usuario in usuariosLista)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == usuario.Id).RoleId;
+                usuario.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+            return Json(new { data = usuariosLista });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BloquearDesbloquear([FromBody] string id)
+        {
+            var usuario = await _unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(u => u.Id == id);
+            if (usuario==null)
+            {
+                return Json(new { success = false, message = "Error de usuario" });
+            }
+            if (usuario.LockoutEnd != null && usuario.LockoutEnd > DateTime.Now)
+            {
+                usuario.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                usuario.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Operacion exitosa" });
+
+        }
+
+        #endregion
+    }
+}
